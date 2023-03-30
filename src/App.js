595d751673e25db36d77eeb4781f6a19c3a2dd43
LeeHongCl <113571786+LeeHongCl@users.noreply.github.com>
2023-03-30 15:54:52 +0900

    Todo Component 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Todo from './Todo';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      item : {id:0, title:"Hello World 1", done:true},
+    };
+  }
   render(){
     return (
-      <div className='App'>
-          <Hello />
+      <div className="App">
+          <Todo item={this.state.item} />
       </div>
     );
   }
