4d9094bd0fbd5136da8ce2389303a922d0a5faa8
LeeHongCl <dlghdcjf76@naver.com>
2023-04-05 15:02:32 +0900

    15 /login 넘기기

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,16 +3,16 @@ import App from "./App";
 import Login from "./Login";
 import{BrowserRouter,Routes,Route} from "react-router-dom";
 import { Box } from "@material-ui/core";
-import Typography from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 function Copyright(){
     return(
         <Typography variant="body2" color="textSecondary" align="center">
             {"Copyright c"}
-            fsoftwareengineer, {new Date().getFullYear}
+            fsoftwareengineer, {new Date().getFullYear()}
             {"."}
         </Typography>
-    )
+    );
 }
 
 class AppRouter extends React.Component{
